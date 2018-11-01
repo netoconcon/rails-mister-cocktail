7dd8eb90bcd505190a7ec6f2d187f8abe0b7a0d2
Neto Concon <concon.neto2@gmail.com>
2018-11-01 18:08:16 -0300

    done cocktail view

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -12,7 +12,7 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render "cocktail/show"
+      render :new
     end
   end
 
